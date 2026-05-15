b07f3edd3aaa25fcb30f0b8f5af8dd520d1b033e
xj63 <xj63work@163.com>
2026-05-15 12:57:04 +0800

    feat: Add tracing and web support
    Adds tracing for logging and enables web support with the `web` feature.
    This includes dependencies for `tracing`, `tracing-wasm`, and
    `console_error_panic_hook`. Also updates `.gitignore` to include `tmp/`
    and `pkg/`.

--- src/web.rs
+++ src/web.rs
@@ -1,1 +1,22 @@
-pub use wasm_bindgen::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    init_logger();
+}
+
+fn init_logger() {
+    // we can call the function at least once during initialization,
+    // and then we will get better error messages if our code ever panics.
+    //
+    // For more details see
+    // https://github.com/rustwasm/console_error_panic_hook#readme
+    console_error_panic_hook::set_once();
+
+    tracing_wasm::set_as_global_default();
+}
+
+#[wasm_bindgen]
+pub fn hello() {
+    tracing::info!("hello");
+}
